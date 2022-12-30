228ced0b44e5a48e3d847ef89ed1d1b94e1171fa
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-12-29 22:38:48 -0500

    Main Class and The Main Method

--- src/Inheritance/Main.java
+++ src/Inheritance/Main.java
@@ -3,5 +3,29 @@ package Inheritance;
 public class Main {
     public static void main(String[] args) {
 
+        Mouse1 m1 = new Mouse1();
+
+        System.out.println("Mouse 1:");
+
+        m1.leftClick();
+        m1.rightClick();
+        m1.scrollUp();
+        m1.scrollDown();
+
+        m1.setColor("red");
+
+        System.out.println("- - - - - - - - - -");
+
+        Mouse2 m2 = new Mouse2();
+
+        System.out.println("Mouse 2:");
+
+        m2.leftClick();
+        m2.rightClick();
+        m2.scrollUp();
+        m2.scrollDown();
+
+        m2.connect();
+
     }
 }

fcea57f6453220889399baadf54a31160951366f
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-12-31 17:34:54 -0500

    Update

---
+++ src/ourclass/main.java
@@ -0,0 +1,13 @@
+package ourclass;
+
+public class main {
+    public static void main(String[] args) {
+
+        Class1 c = new Class1();
+        System.out.println(c.x);
+
+        Class1 ch = new Class1();
+        System.out.println(ch.chars);
+
+    }
+}

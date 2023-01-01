0570963994e48b206cd26a22ae77443cdeb584d7
Jailson Veiga <jmdaveiga2012@hotmail.com>
2023-01-01 18:13:58 -0500

    Update

--- src/abstraction/Abstract.java
+++ src/abstraction/Abstract.java
@@ -1,4 +1,28 @@
 package abstraction;
 
+abstract class Dog {
+
+    public void bark() {
+        System.out.println("Bark!");
+    }
+
+    public abstract void poop();
+}
+
+class Chihuahua extends Dog {
+
+    public void poop() {
+        System.out.println("Dog pooped!");
+    }
+
+}
+
 public class Abstract {
+    public static void main(String[] args) {
+       Chihuahua c = new Chihuahua();
+
+        c.bark();
+
+        c.poop();
+    }
 }

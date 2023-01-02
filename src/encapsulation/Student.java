932f6fb7d589f09728bddbde2896d9663e55353b
Jailson Veiga <jmdaveiga2012@hotmail.com>
2023-01-02 18:35:09 -0500

    Update

--- src/encapsulation/Student.java
+++ src/encapsulation/Student.java
@@ -2,7 +2,22 @@ package encapsulation;
 
 public class Student {
 
-    String name;
-    int age;
+    private String name;
+    private int age;
 
+    public void setName(String newName) {
+        name = newName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(int newAge) {
+        age = newAge;
+    }
+
+    public int getAge() {
+        return age;
+    }
 }

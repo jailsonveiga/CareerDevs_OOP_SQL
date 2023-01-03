5e45fe7252a4e0b13e096b7d410a3ac44e59a1b0
Jailson Veiga <jmdaveiga2012@hotmail.com>
2023-01-03 18:08:14 -0500

    Add properties and a constructor

--- src/project_jay/Contractor.java
+++ src/project_jay/Contractor.java
@@ -1,4 +1,12 @@
 package project_jay;
 
-public class Contractor {
+public class Contractor extends Employee {
+
+    private int workingHours;
+
+    public Contractor(String name, int paymentPerHour, int workingHours) {
+        super(name, paymentPerHour);
+        this.workingHours = workingHours;
+    }
+
 }

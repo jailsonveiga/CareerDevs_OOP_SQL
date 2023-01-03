0f4a9e4b6e341eacbe70fb19224f562036dbf3a8
Jailson Veiga <jmdaveiga2012@hotmail.com>
2023-01-03 18:11:24 -0500

    Update

--- src/project_jay/Employee.java
+++ src/project_jay/Employee.java
@@ -1,6 +1,6 @@
 package project_jay;
 
-public class Employee {
+public abstract class Employee {
 
 
     private String name;
@@ -8,6 +8,21 @@ public class Employee {
 
     public Employee(String name, int paymentPerHour) {
         this.name = name;
+        this.paymentPerHour = paymentPerHour;
+    }
+
+    public abstract int calculateSalary();
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getPaymentPerHour() {
+        return paymentPerHour;
+    }
+    public void setPaymentPerHour(int paymentPerHour) {
         this.paymentPerHour = paymentPerHour;
     }
 
---
+++ src/project_jay/Main.java
@@ -0,0 +1,12 @@
+package project_jay;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Employee contractor = new Contractor("contractor", 10, 10);
+        Employee fullTimeEmployee = new FullTimeEmployee("full time employee", 8);
+        System.out.println(contractor.calculateSalary());
+        System.out.println(fullTimeEmployee.calculateSalary());
+
+    }
+}

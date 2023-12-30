ee4a779ed5d1aa07e9c5d022c0c766c1a7b4dfab
Jailson Veiga <jmdaveiga2012@hotmail.com>
2023-12-30 17:13:07 -0500

    Update

--- src/Polymorphism/Poly.java
+++ src/Polymorphism/Poly.java
@@ -1,4 +1,23 @@
 package Polymorphism;
 
+class Bird {
+    public void sing() {
+        System.out.println("tweet, tweet, tweet");
+    }
+}
+
+class Robin extends Bird{
+    public void sing() {
+        System.out.println("twiddleeeedee");
+    }
+}
+
 public class Poly {
+    public static void main(String[] args) {
+        Bird b = new Bird();
+        b.sing();
+
+        Robin r = new Robin();
+        r.sing();
+    }
 }
